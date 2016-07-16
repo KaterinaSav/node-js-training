59b57bb485df4974caa46a7236a0864fff696a9e
KaterinaSav <caterinasavchuk@gmail.com>
2016-07-16 17:39:27 +0300

    screencast 6

--- user/index.js
+++ user/index.js
@@ -1,11 +1,14 @@
-var phrases = require('./ru');
+var db = require('db');
+var log = require('logger')(module);
 
 function User(name) {
     this.name = name;
 }
 
 User.prototype.hello = function (who) {
-    console.log(phrases.Hello + ", " + who.name);
+    log(db.getPhrase("Hello") + ", " + who.name);
 };
 
-exports.User = User;
+console.log("user.js is required!");
+
+module.exports = User;
