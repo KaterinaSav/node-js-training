b905ee8f442df14e8695621418fd8fcd0edc5118
KaterinaSav <caterinasavchuk@gmail.com>
2016-07-19 23:58:16 +0300

    echo server(screencast15)

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
+// http://127.0.0.1:1337/echo?message=Hello
 var http = require('http');
+var url = require('url');
 
-var server = new http.Server();
+var server = new http.Server(function(req, res) {
+
+  var urlParsed = url.parse(req.url, true);
+
+  if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+    res.setHeader('Cache-control', 'no-cache');
+    res.end( urlParsed.query.message );
+  } else {
+    res.statusCode = 404;
+    res.end("Page not found");
+  }
+});
 
 server.listen(1337, '127.0.0.1');
-
-var counter = 0;
-
-var emit = server.emit;
-server.emit = function(event) {
-  console.log(event);
-  emit.apply(server, arguments);
-};
-
-server.on('request', function(req, res) {
-  res.end("Привет, мир!" + ++counter);
-});
