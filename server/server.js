e025f83316ebf7660224a24aea6a74ef0ed50bc6
KaterinaSav <caterinasavchuk@gmail.com>
2016-07-22 00:15:03 +0300

    debug, winston(screencast19)

--- server/server.js
+++ server/server.js
@@ -1,18 +1,10 @@
 var http = require('http');
-var url = require('url');
+var debug = require('debug')('server');
 
 var server = http.createServer();
 
-server.on('request', function(req, res) {
-  var urlParsed = url.parse(req.url, true);
-
-  if (req.method == 'GET' && urlParsed.pathname == '/echo' && urlParsed.query.message) {
-    res.end( urlParsed.query.message );
-    return;
-  }
-
-  res.statusCode = 404;
-  res.end("Page not found");
-});
+server.on('request', require('./request'));
 
 server.listen(1337);
+
+debug("Server is running");
