29fde050bc50d39cf6153c667349c9ae46664c5e
KaterinaSav <caterinasavchuk@gmail.com>
2016-07-22 00:36:13 +0300

    screencast 20

---
+++ asynchronous-dev/server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var info;
+
+  if (req.url == '/') {
+    fs.readFile('index.html', function(err, info){
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end("На сервере произошла ошибка!");
+        return;
+      }
+
+      res.end(info);
+    });
+  } else {
+    res.end("404");
+  }
+}).listen(3000);
