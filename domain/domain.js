e6d47999ec68832708e833a12a2f736d543dd69d
KaterinaSav <caterinasavchuk@gmail.com>
2016-07-24 23:03:06 +0300

    screencast 28

---
+++ domain/domain.js
@@ -0,0 +1,26 @@
+var domain = require('domain');
+var fs = require('fs'), http = require('http');
+
+var d = domain.create(), server;
+
+d.on('error', function (err) {
+  console.error("Домен перехватил %s", err);
+});
+
+server = new http.Server();
+
+d.run(function () {
+  d.add(server);
+});
+
+console.log(server.domain);
+
+server.on('boom', function() {
+  setTimeout(function() {
+    fs.readFile(__filename, function() {
+      ERROR();
+    });
+  }, 1000);
+});
+
+server.emit('boom');
