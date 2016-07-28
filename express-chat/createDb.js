685e2a5e3ff2152a2610647f7b4512bb81484b32
KaterinaSav <caterinasavchuk@gmail.com>
2016-07-29 00:16:47 +0300

    add mongodb(screencast 4)

--- express-chat/createDb.js
+++ express-chat/createDb.js
@@ -7,20 +7,7 @@ var url = 'mongodb://localhost:27017/chat';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
-  // if(err) throw err;
-  //
-  // var collection = db.collection('test_insert');
-  //
-  // collection.insertOne({a:2}, function(err, docs) {
-  //   collection.count(function (err, count) {
-  //     console.log(format("count = %s", count));
-  //   });
-  //
-  //   collection.find().toArray(function (err, results) {
-  //     console.dir(results);
-  //     db.close();
-  //   });
-  // });
+
   insertDocuments(db, function() {
     db.close();
   });
