dd4d69d8d552b1726d1951a016bd7f62df974942
KaterinaSav <caterinasavchuk@gmail.com>
2016-07-29 00:58:48 +0300

    use mongoose, add users model(screencast 5)

--- express-chat/createDb.js
+++ express-chat/createDb.js
@@ -1,31 +1,14 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+var User = require('./models/user').User;
 
-// Connection URL
-var url = 'mongodb://localhost:27017/chat';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  insertDocuments(db, function() {
-    db.close();
-  });
+var user = new User({
+  username: "Tester",
+  password: "secret"
 });
 
-var insertDocuments = function(db, callback) {
+user.save(function (err, user, affected) {
+  if (err) throw err;
 
-  var collection = db.collection('documents');
-  // Get the documents collection
-  collection.remove();
-  collection.insertOne({a : 2}, function(err, result) {
-    console.log("count = " + result.ops.length);
-    console.log(result.ops);
-    callback(result);
+  User.findOne({username: "Tester"}, function (err, tester) {
+    console.log(tester);
   });
-
-  collection.findOne({a: 2}, function(err, item) {
-    console.log(item);
-    db.close();
-  });
-}
+});
