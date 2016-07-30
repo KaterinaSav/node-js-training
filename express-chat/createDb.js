de354a7d654899b266667b55ae1adb7b12a1809a
KaterinaSav <caterinasavchuk@gmail.com>
2016-07-30 13:33:00 +0300

    use async(screencast 6)

--- express-chat/createDb.js
+++ express-chat/createDb.js
@@ -1,14 +1,44 @@
-var User = require('./models/user').User;
+var mongoose = require('./lib/mongoose');
+var async = require("async");
 
-var user = new User({
-  username: "Tester",
-  password: "secret"
+async.series([
+  open,
+  dropDatabase,
+  requireModels,
+  createUsers
+], function (err) {
+  console.log(arguments);
+  mongoose.disconnect();
+  process.exit(err ? 255 : 0);
 });
 
-user.save(function (err, user, affected) {
-  if (err) throw err;
+function open(callback) {
+  mongoose.connection.on('open', callback);
+}
 
-  User.findOne({username: "Tester"}, function (err, tester) {
-    console.log(tester);
-  });
-});
+function dropDatabase(callback) {
+  var db = mongoose.connection.db;
+  db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+  require('./models/user');
+
+  async.each(Object.keys(mongoose.models), function (modelName, callback) {
+    mongoose.models[modelName].ensureIndexes(callback);
+  }, callback);
+}
+
+function createUsers(callback) {
+
+  var users = [
+    {username: 'Вася', password: 'supervasya0'},
+    {username: 'Вася', password: 'supervasya1'},
+    {username: 'admin', password: 'supervasya2'}
+  ];
+
+  async.each(users, function (userData, callback) {
+    var user = new mongoose.models.User(userData);
+    user.save(callback);
+  }, callback);
+}
