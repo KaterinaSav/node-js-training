1a48e41e49027c55c4bf1adc806c3d3acba9af6a
KaterinaSav <caterinasav@yandex.ru>
2016-08-02 12:48:53 +0300

    configure socket io(screencast 12)

---
+++ express-chat/socket/index.js
@@ -0,0 +1,11 @@
+module.exports = function(server) {
+	var io = require('socket.io')(server);
+	io.set('origins', 'localhost:*');
+	io.on('connection', function (socket) {
+
+		socket.on('message', function (text, cb) {
+			socket.broadcast.emit('message', text);
+			cb();
+		});
+	});
+}
